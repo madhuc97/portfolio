85131766ae2861e45301675c53de0b2819e01474
madhuc97 <mc932099@gmail.com>
2021-05-27 19:20:47 +0530

    update portfolio

---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,387 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const illustration = {
+  animated: true // set to false to use static SVG
+};
+
+const greeting = {
+  username: "Madhu C",
+  title: "Hi all, I'm Madhu",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of designing and building Web and Mobile applications."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1RJSGiB-3p2Nuj5u5jGl9iTUO22dpl_lL/view?usp=sharing",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/madhuc97",
+  linkedin: "https://www.linkedin.com/in/madhu-c-5307b9149",
+  gmail: "mc932099@gmail.com",
+  // gitlab: "https://gitlab.com/",
+  // facebook: "https://www.facebook.com/",
+  medium: "https://medium.com/@davidrakosi",
+  stackoverflow: "https://stackoverflow.com",
+  instagram: 'https://www.instagram.com',
+  twitter: 'https://twitter.com',
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji(
+      "⚡ Create amazing business websites for your business and brands to connect as well as engage with people."
+    ),
+    emoji(
+      "⚡ Expertise in building E-commerce Web and Android Application"
+    )
+    // emoji(
+    //   "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    // )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "HTML-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "CSS3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "Reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "Nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "PHP",
+      fontAwesomeClassname: "fab fa-php"
+    },
+    {
+      skillName: "MySQL-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "AndroidStudio",
+      fontAwesomeClassname: "fab fa-android"
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    {
+      skillName: "Git",
+      fontAwesomeClassname: "fab fa-git"
+    },
+    {
+      skillName: "Firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "PESITM, Shimoga",
+      logo: require("./assets/images/pesitmLogo.jpg"),
+      subHeader: "BE Mechanical Engineering",
+      duration: "August 2015 - June 2019",
+    },
+    {
+      schoolName: "DVS Composite PU Collage, Shimoga",
+      logo: require("./assets/images/dvsLogo.jpg"),
+      subHeader: "PCMB",
+      duration: "June 2013 - April 2015",
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "HTML5", //Insert stack or technology you have experience in
+      progressPercentage: "70%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "CSS",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Javascript",
+      progressPercentage: "50%"
+    },
+    {
+      Stack: "PHP",
+      progressPercentage: "50%"
+    },
+    {
+      Stack: "MySQL",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "WordPress",
+      progressPercentage: "50%"
+    },
+    {
+      Stack: "Android Studio",
+      progressPercentage: "60%"
+    },
+    {
+      Stack: "Java",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Junior SDE",
+      company: "Maveric Digital Systems",
+      companylogo: require("./assets/images/mavericLogo.jpg"),
+      date: "April 2021 – Present",
+      desc:
+        "Java, Springboot"
+    },
+    {
+      role: "Software Engineer",
+      company: "Fackins IT Services",
+      companylogo: require("./assets/images/fackinsLogo.jpg"),
+      date: "May 2020 – March 2021",
+      desc:
+        "PHP, Ecommerce Web and Android Application",
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "drakosi99", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/k3Logo.png"),
+      projectName: "K3 Construction Materials",
+      projectDesc: "It is a complete Ecommerce Website including Admin, Seller and User module, In this project I added all Ecommerce features including Razorpay payment gateway.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "fa"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/dfitLogo.png"),
+      projectName: "D-fit Gym Center",
+      projectDesc: "It is a bussiness website build in wordPress",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://dfit.in.net/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+            ""
+        },
+        {
+          name: "Award Letter",
+          url:
+            ""
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+            ""
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            ""
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+        "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    ""
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    ""
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "Phone No: 7338229913",
+  email_address: "Email ID: mc932099@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "davidrakosi_", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
